725c7ba23b3a9cc1ba57f9f476b99b3dd9076b4c
KYogesh20 <yogesh201201@gmail.com>
2023-02-25 21:06:20 +0530

    amazoff assignment done

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -9,11 +9,55 @@ public class Order {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.id=id;
+        String time[]= deliveryTime.split(":");
+        int hour=Integer.valueOf(time[0]);
+        int min= Integer.valueOf(time[1]);
+        this.deliveryTime = (hour*60) + min;
+
     }
 
     public String getId() {
+
         return id;
     }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
+    public static String getDeliveryTimeAsString(int timeInt){
+        int hours=timeInt/60;
+        int min=timeInt%60;
+        String hrStr="";
+        String minStr="";
+
+        if(hours<10){
+            hrStr="0"+hours;
+        }
+        else{
+            hrStr=""+hours;
+        }
+
+        if(min<10){
+            minStr="0"+min;
+        }
+        else{
+            minStr=""+min;
+        }
+
+        return hrStr+":"+minStr;
+    }
+
+    public static int getDeliveryTimeAsInt(String timeString){
+
+        return (Integer.parseInt(timeString.substring(0,2)) * 60 )+
+                Integer.parseInt(timeString.substring(3) );
+    }
 }
